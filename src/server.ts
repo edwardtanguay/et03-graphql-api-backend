ff5b90494c0fffaffe654b8b275c0daf557f945c
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-31 10:09:59 +0200

    rest api works

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-interface ITest {
-	message: string;
-}
+const exports = {};
+import express from 'express';
 
-const test: ITest = {
-	message: "it works"
-}
+const app = express();
+const PORT = 5424;
 
-console.log(test.message);
+app.get('/', (req, res) => {
+	res.send('REST API and GraphQL Demo Backend');
+})
+
+app.listen(PORT, () => {
+	console.log(`REST API listening on http://localhost:${PORT}`);
+});
