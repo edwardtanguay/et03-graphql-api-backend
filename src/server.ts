48b956ac4d878b45d4b6b9a3d3d0b7414a84992c
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-31 10:30:21 +0200

    graphiql works

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,47 @@
 const exports = {};
 import express from 'express';
 import axios from 'axios';
+import { graphqlHTTP } from 'express-graphql';
+import cors from 'cors';
+import { schema } from './schema.js';
 
 const app = express();
 const PORT = 5424;
 
+app.use(cors());
+
 const employeesUrl = 'https://edwardtanguay.netlify.app/share/employees.json';
+
+// REST API AREA
 
 app.get('/', (req, res) => {
 	res.send('REST API and GraphQL Demo Backend');
-})
+});
 
 app.get('/employees', async (req, res) => {
 	const employees = ((await axios.get(employeesUrl)).data);
 	res.send(employees);
-})
+});
+
+// GRAPHQL AREA
+
+const root = {
+	message: () => {
+		return 'this is the message';
+	}
+};
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		rootValue: root,
+		graphiql: true,
+	})
+);
 
 app.listen(PORT, () => {
 	console.log(`REST API root endpoint: http://localhost:${PORT}`);
 	console.log(`REST API employees endpoint: http://localhost:${PORT}/employees`);
+	console.log(`GRAPHQL API employees endpoint: http://localhost:${PORT}/graphql`);
 });
