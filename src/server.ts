0e0aefc9edf2be015cdb44927455802fe4c086bb
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-31 10:15:55 +0200

    rest api employees works

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 const exports = {};
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 const PORT = 5424;
+
+const employeesUrl = 'https://edwardtanguay.netlify.app/share/employees.json';
 
 app.get('/', (req, res) => {
 	res.send('REST API and GraphQL Demo Backend');
 })
 
+app.get('/employees', async (req, res) => {
+	const employees = ((await axios.get(employeesUrl)).data);
+	res.send(employees);
+})
+
 app.listen(PORT, () => {
-	console.log(`REST API listening on http://localhost:${PORT}`);
+	console.log(`REST API root endpoint: http://localhost:${PORT}`);
+	console.log(`REST API employees endpoint: http://localhost:${PORT}/employees`);
 });
